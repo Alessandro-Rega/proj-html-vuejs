5a213a6b304fab93fcc44f6b0bb053c39bcd4b1a
Alessandro-Rega <regaale47@gmail.com>
2022-01-12 12:19:41 +0100

    insert hero

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <Header v-bind:vociMenu="vociMenu"/>
+    <Hero/>
   </div>
 </template>
 
 <script>
 
-import Header from "./components/Macro/Header.vue"; 
+import Header from "./components/Macro/Header.vue";
+import Hero from "./components/Macro/Hero.vue"; 
 
 export default {
   name: 'App',
   components: {
     Header,
+    Hero,
   },
   data(){
     return{
---
+++ src/components/Macro/Hero.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="hero">
+      <h1>building inspiring spaces</h1>
+      <p>We Build inspiring Residential & Commercial Spaces</p>
+      <div>
+        <button class="btn">explore recent work</button>
+        <button class="btn">get a quote today</button>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Hero',
+    components: {
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+// Importazione variabili
+@import "../../assets/style/variables.scss";
+
+.hero{
+    // Sezione sfondo
+    width: 100%;
+    height: 790px;
+    background-image: url("../../assets/img/jumbotron.png");
+    background-size: 100%;
+    background-repeat: no-repeat;
+    // --
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    p{
+        font-size: 20px;
+        color: $giallo;
+        margin: 20px 0;
+        font-weight: bold;
+    }
+
+    button{
+        font-size: 18px;
+        padding: 18px 50px;
+        margin: 0 20px;
+    }
+
+    button:first-of-type{
+        background-color: $nero;
+        color: $bianco;
+    }
+
+    button:last-of-type{
+        background-color: $giallo;
+        color: $nero;
+    }
+}
+</style>
